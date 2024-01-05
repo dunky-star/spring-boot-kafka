420387c2c096cd90d14a87fa8e61bcb390b41147
dunky-star <dunkygeoffrey39@gmail.com>
2024-01-04 21:11:39 -0600

    First commit

---
+++ src/test/java/com/dunky/springkafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunky.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
