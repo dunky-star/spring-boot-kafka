40cd602e6190f41be1bbe1d2eafea5c8d75444c5
dunky-star <dunkygeoffrey39@gmail.com>
2024-01-04 21:51:00 -0600

    Kafka Topic Bean Configuration

--- src/main/java/com/dunky/springkafka/config/KafkaTopicConfig.java
+++ src/main/java/com/dunky/springkafka/config/KafkaTopicConfig.java
@@ -1,7 +1,15 @@
 package com.dunky.springkafka.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
+    @Bean
+    public NewTopic dunkyjavaTopic(){
+        return TopicBuilder.name("dunkyjava")
+                .build();
+    }
 }
