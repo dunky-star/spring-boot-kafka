8a110f784646dc547cf6d421a5688e1e1fb7f334
dunky-star <dunkygeoffrey39@gmail.com>
2024-01-04 22:15:17 -0600

    Kafka Topic, producer and MessageController

---
+++ src/main/java/com/dunky/springkafka/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.dunky.springkafka.controller;
+
+import com.dunky.springkafka.kafka.KafkaProducer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+    private final KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    
+}
--- src/main/java/com/dunky/springkafka/kafka/KafkaProducer.java
+++ src/main/java/com/dunky/springkafka/kafka/KafkaProducer.java
@@ -1,10 +1,13 @@
 package com.dunky.springkafka.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
@@ -12,6 +15,7 @@ public class KafkaProducer {
     }
 
     public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent -> %s ", message));
         kafkaTemplate.send("dunkyjava", message);
     }
 }
