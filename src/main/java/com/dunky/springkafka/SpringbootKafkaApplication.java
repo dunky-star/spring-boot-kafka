d26efda94c7a5598c01a8b1cc3e6d2c1df24656a
dunky-star <dunkygeoffrey39@gmail.com>
2024-01-04 22:41:54 -0600

    Kafka Topic, producer and MessageController

--- src/main/java/com/dunky/springkafka/SpringbootKafkaApplication.java
+++ src/main/java/com/dunky/springkafka/SpringbootKafkaApplication.java
@@ -2,6 +2,9 @@ package com.dunky.springkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class SpringbootKafkaApplication {
@@ -9,5 +12,6 @@ public class SpringbootKafkaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootKafkaApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/dunky/springkafka/controller/MessageController.java
+++ src/main/java/com/dunky/springkafka/controller/MessageController.java
@@ -1,7 +1,10 @@
 package com.dunky.springkafka.controller;
 
 import com.dunky.springkafka.kafka.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +16,10 @@ public class MessageController {
         this.kafkaProducer = kafkaProducer;
     }
 
-    
+    // http://localhost:8080/api/v1/kafka/publish?message=hello Kafka
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to the topic");
+    }
 }
