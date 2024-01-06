1d063024826ebf6d37f57259eaf4c7ea928409a2
dunky-star <dunkygeoffrey39@gmail.com>
2024-01-05 21:12:43 -0600

    Kafka Topic, producer, MessageController, POJO, JsonSerializer, JsonDeserializer and consumer

--- src/main/java/com/dunky/springkafka/kafka/JsonKafkaConsumer.java
+++ src/main/java/com/dunky/springkafka/kafka/JsonKafkaConsumer.java
@@ -8,7 +8,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 public class JsonKafkaConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
 
-    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId="myGroup")
+    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId="${spring.kafka.consumer.group-id}")
     public void consume(User user){
         LOGGER.info(String.format("Json message received -> %s", user.toString()));
     }
